ffb42500de61128df0ac0178f908b53bf9d76e42
Daniel Morshinin <geelix@mail.ru>
2025-06-20 18:18:04 +0300

    feat: add validation pipe and user dto

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, HttpException, HttpStatus } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      exceptionFactory: () => {
+        throw new HttpException(
+          {
+            statusCode: HttpStatus.METHOD_NOT_ALLOWED,
+            message: 'Invalid input',
+          },
+          HttpStatus.METHOD_NOT_ALLOWED,
+        );
+      },
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
