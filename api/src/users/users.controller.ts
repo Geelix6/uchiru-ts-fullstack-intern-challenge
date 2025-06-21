6022dc9ed4b5e1ab60dc6b9e8c509db32347355a
Daniel Morshinin <geelix@mail.ru>
2025-06-21 20:33:58 +0300

    feat: implement user registration with password hashing

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Res, HttpStatus } from '@nestjs/common';
+import { Response } from 'express';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { generateToken } from 'src/utils/generate-token.util';
 
 @Controller('user')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  async newUser(@Body() dto: CreateUserDto, @Res() res: Response) {
+    const user = await this.usersService.create(dto.login, dto.password);
+    const token = generateToken(user.id);
+
+    res.set('X-Auth-Token', token);
+    return res.status(HttpStatus.CREATED).send();
+  }
 }
--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly repo: Repository<User>,
+  ) {}
+
+  async create(login: string, plainPassword: string): Promise<User> {
+    const exists = await this.repo.findOne({ where: { login } });
+
+    if (exists) {
+      throw new ConflictException('User with this login already exists');
+    }
+
+    const passwordHash = await bcrypt.hash(plainPassword, 10);
+    const user = this.repo.create({ login, password: passwordHash });
+
+    return this.repo.save(user);
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.repo.find();
+  }
+}
