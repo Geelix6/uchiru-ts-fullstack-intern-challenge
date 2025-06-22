575d8385817fb6fe5b601b7dad4a927a30280f2f
Daniel Morshinin <geelix@mail.ru>
2025-06-22 17:59:16 +0300

    chore: remove Swagger UI

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,5 @@
-import { readFileSync } from 'fs';
-import { join } from 'path';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import * as yaml from 'js-yaml';
-import * as swaggerUi from 'swagger-ui-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -15,12 +11,6 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-
-  const specPath = join(__dirname, '../../openapi.yaml');
-  const spec = yaml.load(readFileSync(specPath, 'utf8'));
-
-  app.use('/docs', swaggerUi.serve, swaggerUi.setup(spec));
-  console.log('Swagger UI available at /docs');
 
   await app.listen(3000);
 }
